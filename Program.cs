5a616fcba836035918be064626bb20fa7cc5407d
Tina Dam <124227045+dtsunai@users.noreply.github.com>
2023-04-26 21:30:09 -0700

    Twitter.cs
    HW6ACH4 - ISDS 309
    2) Write a program named Twitter that accepts a user’s message and determines whether it is short enough for a social networking service that does not accept messages of more than 140 characters.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿//Write a program named Twitter that accepts a user’s message and determines
+//whether it is short enough for a social networking service that does not accept
+//messages of more than 140 characters.
+
+using System;
+class Twitter
+{
+    static void Main()
+    {
+        string input, like, comment, share;
+
+        like = "Like";
+        comment = "Comment";
+        share = "Share";
+
+        Console.WriteLine("Welcome to Twitter!");
+        Console.Write("What's happening? ");
+        input = Console.ReadLine();
+
+        if (input.Length > 140)
+        {
+            Console.WriteLine("\nMessage exceeds 140 characters.");
+            Console.WriteLine("Your post had {0} too many characters. Please close this window to try again.", input.Length - 140);
+        }
+        else
+        {
+            Console.WriteLine("\nPost created!");
+            Console.WriteLine("User: {0}", input);
+
+            Console.WriteLine("\n{0,-10}{1,6}{2, 11}", like, comment, share);
+        }
+    }
+}
